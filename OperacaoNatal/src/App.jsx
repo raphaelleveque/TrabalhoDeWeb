d574b486a0da6f104f829b852661a0728848f137
Lusca <lucaspacheco747@gmail.com>
2024-04-19 00:35:19 -0300

    Migrando para react

--- OperacaoNatal/src/App.jsx
+++ OperacaoNatal/src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link
+} from "react-router-dom"
+import Informacoes from './components/informacoes'
+import Sobre from "./components/sobre";
+import Contato from "./components/contato";
 import './App.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/Home",
+    element: <Informacoes/>
+  },
+  {
+    path: "/Sobre",
+    element: <Sobre/>
+  },
+  {
+    path: "/Contribuicoes",
+    element: <Contato/>
+  },
+  {
+    path: "/Informacoes",
+    element: <Informacoes/>
+  },
+  {
+    path: "/Login",
+    element: <Informacoes/>
+  }
+
+]);
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <RouterProvider router={router}/>
+
+      {/* <img className="ufo" id="mascote1" src="src/visual/Nicolau (1).png" alt=""/>
+      <img className="ufo" id="mascote2"src="src/visual/Nicolau_Sino.png" alt=""/>
+      <img className="ufo" id="mascote3"src="src/visual/Natalina.png" alt=""/>
+      <img className="ufo" id="mascote4"src="src/visual/Floquinho (1).png" alt=""/>
+      <img className="ufo" id="mascote5" src="src/visual/Bell (2).png" alt=""/> */}
+
+      <footer>
+        <p>Operação Natal ©2024</p>
+      </footer>
     </>
   )
 }
