5edc14d4b4c11202c15958f0a8d877472c4dfa74
Henrique Bovo <henrique.bovo@usp.br>
2024-05-10 11:24:09 -0300

    Adicionando tela de login

--- TrabalhoDeWeb/OperacaoNatal/src/components/Login/index.jsx
+++ TrabalhoDeWeb/OperacaoNatal/src/components/Login/index.jsx
@@ -1,37 +1,44 @@
-import style from "./Login.module.css"
+import Style from "./Login.module.css"
 import Header from "../header"
 
 
-function Login(){
-    return(
+function Login() {
+  return (
     <>
-        <Header/>
-        <div class="informacoes">
-            <div class="border">
-                <div class="evento">
-                    <div class="conteudo">
-                        <div class="formulario-container">
-                            <h2>Entre em sua conta</h2>
-                            <form id="formao500" name="formao500" action="" method="post">
-                                <label for="email">E-mail:</label>
-                                <input type="email" name="email" maxlength="100"
-                                    placeholder="fulano.de.tal@email.com"
-                                    required />
-                
-                                <label for="senha">Senha:</label><br />
-                                <input type="password" name="senha" Maxlength="30"/>
-                
-                                <input type="hidden" name="formulario_login" value={1} required />
-                                <input type="submit" name="botao" value="Logar" />
-                            </form>
-                        </div>
-                    </div>
-                </div>
+      <Header />
+      <div className={Style.info}>
+        <h2>Entre em sua conta:</h2>
+      </div>
+      <div className={Style.informacoes}>
+        <div className={Style.border}>
+          <div className={Style.evento}>
+            <div className={Style.conteudo}>
+              <div className={Style.formulario}>
+
+                <form id="formao500" name="formao500" action="" method="post" style={{ textAlign: "center" }}>
+                  <label for="email">E-mail:</label><br />
+                  <input type="email" name="email" maxlength="100"
+                    placeholder="Digite seu e-mail"
+                    required /><br /><br />
+
+                  <label for="senha">Senha:</label><br />
+                  <input type="password" name="senha" Maxlength="30"
+                    placeholder="Digite sua senha"
+                  /><br /><br />
+
+                  <input type="hidden" name="formulario_login" value={1} required />
+                  <input className={Style.botao} type="submit" name="botao" value="Logar" />
+                </form>
+                <p className={Style.cadastro}>Ainda não tem uma conta? <a href="/cadastro">Cadastre-se aqui</a>.</p>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </>
-    );
+  );
 
 };
+
 
 export default Login;
--- TrabalhoDeWeb/OperacaoNatal/src/components/informacoes/index.jsx
+++ TrabalhoDeWeb/OperacaoNatal/src/components/informacoes/index.jsx
@@ -5,55 +5,55 @@ import img3 from "../../assets/img3.jpg"
 
 import Header from "../header"
 
-function Informacoes(){
-    return(
-        <>
-        <Header/>
-        <section id="Conteudo">
+function Informacoes() {
+  return (
+    <>
+      <Header />
+      <section id="Conteudo">
 
-            <div className="intro">
-                <h2>Informações</h2>
-                <h3>Fique Antenado nos próximos eventos!</h3>
+        <div className={Style.info}>
+          <h2>Informações</h2>
+          <h3>Fique Antenado nos próximos eventos!</h3>
+        </div>
+
+        <div className={Style.informacoes}>
+          <div className={Style.border}>
+            <div className={Style.evento}>
+              <img src={img1} alt="Equipe da operação natal com crianças" />
+              <div className={Style.conteudo}>
+                <h4>Dia D</h4>
+                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <button>Saiba mais</button>
+              </div>
             </div>
+          </div>
 
-            <div className={Style.informacoes}>
-                <div className={Style.border}>
-                    <div className={Style.evento}>
-                        <img src={img1} alt="Equipe da operação natal com crianças"/>
-                        <div className={Style.conteudo}>
-                            <h4>Dia D</h4>
-                            <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
-                            <button>Saiba mais</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={Style.border}>
-                    <div className={Style.evento}>
-                        <img src={img2} alt="Criança sendo maquiada por uma organizadora da Operação Natal"/>
-                        <div className={Style.conteudo}>
-                            <h4>Festinha divina providência</h4>
-                            <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
-                            <button>Saiba mais</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={Style.border}>
-                    <div className={Style.evento}>
-                        <img src={img3} alt="Papai Noel com crianças"/>
-                        <div className={Style.conteudo}>
-                            <h4>Festinha Doces Flautistas</h4>
-                            <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
-                            <button>Saiba mais</button>
-                        </div>
-                    </div>
-                </div>
+          <div className={Style.border}>
+            <div className={Style.evento}>
+              <img src={img2} alt="Criança sendo maquiada por uma organizadora da Operação Natal" />
+              <div className={Style.conteudo}>
+                <h4>Festinha divina providência</h4>
+                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <button>Saiba mais</button>
+              </div>
             </div>
-        </section>
+          </div>
 
-        </>
-    );
+          <div className={Style.border}>
+            <div className={Style.evento}>
+              <img src={img3} alt="Papai Noel com crianças" />
+              <div className={Style.conteudo}>
+                <h4>Festinha Doces Flautistas</h4>
+                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <button>Saiba mais</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  );
 }
 
 export default Informacoes;
