1a9177931ed7e8f342197914fb79e9782062122f
Lusca <lucaspacheco747@gmail.com>
2024-05-14 20:27:19 -0300

    corrigindo texto informações

--- TrabalhoDeWeb/OperacaoNatal/src/components/informacoes/index.jsx
+++ TrabalhoDeWeb/OperacaoNatal/src/components/informacoes/index.jsx
@@ -22,7 +22,7 @@ function Informacoes() {
               <img src={img1} alt="Equipe da operação natal com crianças" />
               <div className={Style.conteudo}>
                 <h4>Dia D</h4>
-                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <article>Dia D é o evento em que reunimos o máximo de pessoas para irmos às ruas para arrecadar doações que serão distruidas para uma série de instiuições que precisem desse auxílio.</article>
                 <button>Saiba mais</button>
               </div>
             </div>
@@ -33,7 +33,7 @@ function Informacoes() {
               <img src={img2} alt="Criança sendo maquiada por uma organizadora da Operação Natal" />
               <div className={Style.conteudo}>
                 <h4>Festinha divina providência</h4>
-                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <article>Divina providência é uma creche que trabalhamos na qual arrecadamos sacolinhas para os alunos e distribuimos nas festinhas juntamente de um evento recheado de brincadeiras e interações com os pequenos.</article>
                 <button>Saiba mais</button>
               </div>
             </div>
@@ -44,7 +44,7 @@ function Informacoes() {
               <img src={img3} alt="Papai Noel com crianças" />
               <div className={Style.conteudo}>
                 <h4>Festinha Doces Flautistas</h4>
-                <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi esse amet tenetur rem voluptatum recusandae id ut eos culpa sed in beatae fuga minima delectus autem, accusantium possimus sint quod.</article>
+                <article>Doces flautistas é um projeto social que visa transformação social através da música. Nas festinhas, realizamos as doações das sacolinhas arrecadas e realizamos o evento recreativo com elas!</article>
                 <button>Saiba mais</button>
               </div>
             </div>
