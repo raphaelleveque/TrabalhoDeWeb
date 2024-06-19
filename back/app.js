f307fa80823dd51d15d97558faa43a5c8b990853
Raphael Leveque <raphaellev1102@gmail.com>
2024-06-19 00:19:05 -0300

    Criar Usuario

--- back/app.js
+++ back/app.js
@@ -7,13 +7,27 @@ app.use(cors());
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
+    user: "root",
+    password: "senha"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Banco ok!");
+});
+
+app.post('/executar-procedure', (req, res) => {
+    const nomeProcedure = 'minha_procedure';
+    const parametro1 = req.query.parametro1;
+    const parametro2 = req.query.parametro2;
+
+    db.query(CALL CriarUsuario(?, ?), [parametro1, parametro2], (err, result) => {
+        if (err) {
+            console.error('Erro ao executar a procedure:', err);
+            return res.status(500).send('Erro ao executar a procedure');
+        }
+        res.json(result);
+    });
 });
 
 
@@ -24,6 +38,20 @@ app.post('/', (req,res) => {
     var xx = req.body
     res.send('Hello world');
 });
+
+app.post('/CriaUsuario', (req,res) => {
+    var email, senha = req.body
+
+    db.query("CALL CriarUsuario(?, ?)", [senha, email], (err, result) => {
+        if (err) {
+            console.error('Erro ao executar a procedure:', err)
+            return res.status(500).send('Erro ao executar a procedure')
+        }
+        res.json(result)
+    })
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server rodando')
